d1e94de270adeded9dd4c687f7d6d5c1ace03df2
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-31 15:46:04 +0530

    Welcome to the Gambling Simulation java Program

---
+++ GamblingSimulation.java
@@ -0,0 +1,10 @@
+package GamblingSimulation;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome to the Gambling Simulation Program.");
+	}
+
+}
