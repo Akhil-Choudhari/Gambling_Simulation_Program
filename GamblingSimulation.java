0da26740ce42b84bbe70b0c1ae490ea6c16be92e
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-31 15:56:12 +0530

    Entered Total Stakes and each Game Bit.

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -2,9 +2,14 @@ package GamblingSimulation;
 
 public class GamblingSimulation {
 
+	static int TotalDayStake = 100;
+	static int EachBit = 1;
+	
 	public static void main(String[] args) {
 		
 		System.out.println(" Welcome to the Gambling Simulation Program.");
+		System.out.println("Total Stakes of a day is 100 $");
+		System.out.println("Bit of Each Game is 1 $");
 	}
 
 }
